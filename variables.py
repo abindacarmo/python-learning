5fe60b7b9728849e9edb8e97e8ed1569ce7f7899
abindacarmo <abindacarmo08@gmail.com>
2026-03-11 10:35:52 +0900

    aumenta file variable(esplikasaun konaba variable iha pyhton)

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Variable mak hanesan fatin ida ne'ebe rai dados iha programa
+# No iha python ita la presiza identifika tipo de dados hanesan LP sira seluk, tamba python sei identifika ho otomatis
+
+naran = "Dito" #String
+idade = 19 #integer
+estudante = True #Boolean
+altura_dito = 176.0 #Float
+
+# OINSA ITA BELE HAKEREK VARIABLE HO LOS
+# MANEIRA HAKEREK SALA:
+# Naran = "Dito"
+# 2idade = 19
+# atura dito = 176.0
+
+# MANEIRA HAKEREK LOS:
+# naran = "Dito"
+# altura_dito = 176.0
+# _idade = 19
+
+
+print(naran) #tamba variable naran defini ona entaun ita la presiza fo sai liafuan ne'e uza "Dito" maibe ita bele bolu direta variable naran laho aspa (""), hanesan mos sira seluk
+print(idade)
+print(estudante)
+print("Dito nia altura mak:", altura_dito) #karik ita hakarak aumenta liafuan ruma antes variable mak ita sei uza conkatenasaun(,) antes variable.
+
