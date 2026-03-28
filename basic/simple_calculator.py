7bc7fa67fa3ee3d716b678f40757df2846ef875a
abindacarmo <abindacarmo08@gmail.com>
2026-03-28 13:29:54 +0900

    aumenta while loop iha simple_calculetor and update  treinu while loop

--- basic/simple_calculator.py
+++ basic/simple_calculator.py
@@ -1,40 +1,61 @@
 # tuir mai sei aprende halo calculator basika ida ne'ebe iha operasaun haat: (+, -, *, /)
 
 # tuir mai ita sei uza function input() mak hanesan built-in fuction(funsaun ne'ebe iha nanis ona wainhira ita utiliza python) ne'ebe ita bele prense buat ruma ho manual
-a = int(input("input numeru 1: ")) # ita uza int() atu nune'e bele input numeru integer laos string, karik ita la uza int numeru ne'ebe ita prense sei sai string automatikamente
-b = int(input("input numeru 2: "))
-operasaun = input("hili operasaun(+, -, *, /): ")
 
-def mais(a, b):
-    return a + b
-
-
-def menus(a, b):
-    return a - b
+def calcula():
+    a = int(input("input numeru 1: ")) # ita uza int() atu nune'e bele input numeru integer laos string, karik ita la uza int numeru ne'ebe ita prense sei sai string automatikamente
+    b = int(input("input numeru 2: "))
+    operasaun = input("hili operasaun(+, -, *, /): ")
 
 
-def vezes(a, b):
-    return a * b
+    def mais(a, b):
+        return a + b
 
 
-def dividir(a, b):
-    return a / b
+    def menus(a, b):
+        return a - b
 
 
+    def vezes(a, b):
+        return a * b
 
-if operasaun == "+":
-    maiss = mais(a, b)
-    print("resustado: ", maiss)
 
-elif operasaun == "-":
-    menuss = menus(a, b)
-    print("resustado: ", menuss)
+    def dividir(a, b):
+        return a / b
 
-elif operasaun == "-":
-    vezess = vezes(a, b)
-    print("resustado: ", vezess)
 
-else:
-    dividirr = dividir(a, b)
-    print("resultado: ", dividirr)
+    if operasaun == "+":
+        maiss = mais(a, b)
+        print("resustado: ", maiss)
+
+    elif operasaun == "-":
+        menuss = menus(a, b)
+        print("resustado: ", menuss)
+
+    elif operasaun == "-":
+        vezess = vezes(a, b)
+        print("resustado: ", vezess)
+
+    else:
+        dividirr = dividir(a, b)
+        print("resultado: ", dividirr)
+
+calcula()
+
+while True:
+
+    hili = input("hakarak kontinua: YES or NO:")
+
+    if hili == "y":
+        calcula()
+
+    elif hili == "n":
+        break
+
+    else:
+        print("opsaun invalidu!")
+
+    
+
+
 
