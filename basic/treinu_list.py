96eadb74a1960f49d8311cb0584e38d0864a7070
abindacarmo <abindacarmo08@gmail.com>
2026-03-28 15:08:58 +0900

    aumenta(basic) aumenta file treinu list

---
+++ basic/treinu_list.py
@@ -0,0 +1,9 @@
+# LIST - halo lista ne'ebe kolega nain 5 nia naran iha laran, no print ida por ida uza for loop
+
+naran_kolega = ["Selfiana", "Acacio", "Ezequiel", "Inope", "Ary", "Ediko"]
+
+numero = 1
+for i in naran_kolega:
+    print("kolega numeru", numero, "mak: ", i)
+
+    numero += 1
